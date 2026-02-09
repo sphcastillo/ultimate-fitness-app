4fbc2a7ad6890db1dff1f061d266d7c2a4df5ca4
sphcastillo <sphcastillo@gmail.com>
2026-02-09 15:57:04 -0500

    Refactor app layout to use ClerkProvider and implement new tab structure with updated screens for exercises, workout, history, and profile

---
+++ src/app/(app)/(tabs)/_layout.tsx
@@ -0,0 +1,75 @@
+import { Tabs } from 'expo-router';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+export default function Layout() {
+    return (
+        <Tabs>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    headerShown: false,
+                    title: 'Home',
+
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign name="home" color={color} size={size} />
+                    )
+                }}
+            />
+
+            <Tabs.Screen
+                name='exercises'
+                options={{
+                    title: 'Exercises',
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign name="book" color={color} size={size} />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name='workout'
+                options={{
+                    title: 'Workout',
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign name="plus-circle" color={color} size={size} />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name='active-workout'
+                options={{
+                    title: 'Active Workout',
+                    headerShown: false,
+                    href: null,
+                    tabBarStyle: { display: 'none' },
+                }}
+            />
+            <Tabs.Screen
+                name='history'
+                options={{
+                    title: 'History',
+                    headerShown: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign name="clock-circle" color={color} size={size} />
+                    )
+                }}
+            />
+
+            <Tabs.Screen
+                name='profile'
+                options={{
+                    title: 'Profile',
+                    headerShown: false,
+                    // tabBarIcon: ({ color, size }) => (
+                    //     <Image 
+                    //         source={user?.imageUrl ?? user?.externalAccounts[0]?.imageUrl}
+                    //         className='rounded-full'
+                    //         style={{ width: 28, height: 28, borderRadius: 100 }}
+                    //     />
+                    // ),
+                }}
+            />
+        </Tabs>
+    )
+}
